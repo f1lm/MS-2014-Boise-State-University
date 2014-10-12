cd6a715a9976a094d045de6ddbca932dd6890540
Milstein Munakami <milsonmun@yahoo.com>
2014-10-12 10:42:39 -0600

    Added Queue

--- Cloud Computing/Assignments/Assignment-1/socialnetworking/src/main/java/socialnetworking/queue/MonitorService.java
+++ Cloud Computing/Assignments/Assignment-1/socialnetworking/src/main/java/socialnetworking/queue/MonitorService.java
@@ -1,19 +1,36 @@
 package socialnetworking.queue;
 
-import java.util.ArrayList;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-import socialnetworking.library.DataModel;
-
-import com.google.gson.Gson;
+import socialnetworking.rest.App;
 
 @Path("/monitor")
 public class MonitorService {
+
 	private final static String queueName = "processing-queue";
+
+	/**
+	 * Root resource (exposed at "myresource" path)
+	 */
+	@Path("colo")
+	public class HelloWorld {
+
+		/**
+		 * Method handling HTTP GET requests. The returned object will be sent
+		 * to the client as "text/plain" media type.
+		 * 
+		 * @return String that will be returned as a text/plain response.
+		 */
+		@GET
+		@Produces(MediaType.TEXT_PLAIN)
+		public String serveRequest() {
+			return "Request was served by : " + App.BASE_URI + "colo/";
+		}
+	}
 
 	@GET
 	@Path("/processingtime")
