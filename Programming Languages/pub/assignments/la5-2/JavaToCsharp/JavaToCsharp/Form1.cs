4ba5c7fe61dcfe8cf7ceaa7609e65c670baad0a8
Milstein Munakami <milsonmun@yahoo.com>
2014-11-12 17:56:49 -0700

    added C# COde for assignement

---
+++ Programming Languages/pub/assignments/la5-2/JavaToCsharp/JavaToCsharp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JavaToCsharp
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        private string label1, label2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1 = "off";
+            label2 = "on";
+            btnClick.Text = label1;
+            lblText.Text = Convert.ToString(count, 2);
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            String s = label1;
+            label1 = label2;
+            label2 = s;
+            btnClick.Text = label1;
+
+            lblText.Text = Convert.ToString(++count, 2);         
+        }
+    }
+}
