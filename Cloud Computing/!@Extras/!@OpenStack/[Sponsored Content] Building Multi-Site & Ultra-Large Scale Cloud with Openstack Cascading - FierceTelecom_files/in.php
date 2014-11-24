a74010e3dede7c7f37640b3f2ff7207e694ea1ce
Milstein Munakami <milsonmun@yahoo.com>
2014-11-24 12:55:46 -0700

    Nov 24

---
+++ Cloud Computing/!@Extras/!@OpenStack/[Sponsored Content] Building Multi-Site & Ultra-Large Scale Cloud with Openstack Cascading - FierceTelecom_files/in.php
@@ -0,0 +1,4 @@
+// static24
+if( window._genericStats ) _genericStats.set_cookie('_jsuid', '1860847863');if( window._genericStats ) _genericStats.set_cookie('_utm_og', '%26utm_campaign%3DAddThis%26utm_medium%3DAddThis%26utm_source%3Dlinkedin', 90*86400 );
+// exit utm
+var o_O
