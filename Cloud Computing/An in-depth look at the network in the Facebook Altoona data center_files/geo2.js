9d6a7d8a03686875f5ae25727d234e1b21911ed7
Milstein Munakami <milsonmun@yahoo.com>
2014-12-12 13:51:11 -0700

    updated Dec 12

---
+++ Cloud Computing/An in-depth look at the network in the Facebook Altoona data center_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "BOISE",
+      'continent': "NA",
+      'country': "US",
+      'region': "ID"
+    },
+    'ip':"132.178.9.19"
+  }]);
+})
+//
+()
+
+;
