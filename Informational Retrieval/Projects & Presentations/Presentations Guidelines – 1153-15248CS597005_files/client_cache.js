dbc75f800bd5bcbeec5b4dd0167566fe1bc5da58
Milstein Munakami <milsonmun@yahoo.com>
2015-01-26 13:07:30 -0700

    updated

---
+++ Informational Retrieval/Projects & Presentations/Presentations Guidelines – 1153-15248CS597005_files/client_cache.js
@@ -0,0 +1,40 @@
+var CLIENT_CACHE_GUID_COOKIE_NAME = 'web_client_cache_guid';
+
+var ClientCache =
+{
+    clear : function()
+    {
+      sessionStorage.clear();
+    },
+
+    setItem : function( key, value )
+    {
+      this._validateSession();
+      sessionStorage.setItem( key, value );
+    },
+    
+    getItem : function(key)
+    {
+      this._validateSession();
+      return sessionStorage.getItem( key );
+    },
+
+    removeItem : function( key )
+    {
+      this._validateSession();
+      sessionStorage.removeItem( key );
+    },
+
+    _validateSession : function()
+    {
+      var guidCookie = getCookie( CLIENT_CACHE_GUID_COOKIE_NAME );
+      if ( guidCookie == null || guidCookie != sessionStorage.getItem( CLIENT_CACHE_GUID_COOKIE_NAME ) )
+      {
+        sessionStorage.clear();
+        if ( guidCookie != null )
+        {
+          sessionStorage.setItem( CLIENT_CACHE_GUID_COOKIE_NAME, guidCookie );
+        }
+      }
+    }
+};
