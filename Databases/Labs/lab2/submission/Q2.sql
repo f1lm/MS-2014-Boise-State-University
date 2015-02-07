7455ef29354de81166e5afceac8bd2358c1d8a2b
Milstein Munakami <milsonmun@yahoo.com>
2015-02-07 16:01:01 -0700

    update

---
+++ Databases/Labs/lab2/submission/Q2.sql
@@ -0,0 +1,5 @@
+-- 2. Retrieve the names of all students who didn’t take any course from the computer science department.
+select s.FName, s.LName from STUDENT s Where s.SSN NOT IN (
+select s1.SSN from STUDENT s1 INNER join GRADE_REPORT g on s1.SSN = g.SSN
+INNER JOIN COURSE c ON c.CourseNo = g.CourseNo 
+And c.DeptCode = (select DeptCode from DEPT d Where d.DName='Computer Science'));
