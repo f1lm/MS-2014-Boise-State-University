25b48233a61c9690c32b9602ad131a8567c17941
Milstein Munakami <milsonmun@yahoo.com>
2015-02-18 18:11:21 -0700

    updated

---
+++ Informational Retrieval/Worldwide flights visualization shows how busy our skies really are   Geek-Cetera   Geek.com_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0","multiple":"{num}","one":"1"}},"counts":[{"id":"1616053 http:\/\/www.geek.com\/featured\/-1616053\/","comments":2}]});
+}
