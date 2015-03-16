f4aa66b396fc424847e7b385fad981a855341006
Milstein Munakami <milsonmun@yahoo.com>
2015-03-16 11:39:47 -0600

    updated

--- Databases/Lab Assignments/lab1/files/load-data-university.sql
+++ Databases/Lab Assignments/lab1/files/load-data-university.sql
@@ -2,33 +2,33 @@ use `university`;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/course.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/course.unl' 
 INTO TABLE COURSE
 FIELDS TERMINATED BY '|' ;
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/dept.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/dept.unl' 
 INTO TABLE DEPT
 FIELDS TERMINATED BY '|';
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/grade_report.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/grade_report.unl' 
 INTO TABLE GRADE_REPORT
 FIELDS TERMINATED BY ' ';
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/major.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/major.unl' 
 INTO TABLE MAJOR
 FIELDS TERMINATED BY ' ';
 
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/minor.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/minor.unl' 
 INTO TABLE MINOR
 FIELDS TERMINATED BY ' ';
 
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/section.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/section.unl' 
 INTO TABLE SECTION
 FIELDS TERMINATED BY ' ';
 
-LOAD DATA LOCAL INFILE 'E:/MS-2014-Boise-State-University/Databases/Labs/lab1/files/student.unl' 
+LOAD DATA LOCAL INFILE 'D:/MS-2014-Boise-State-University/Databases/Lab Assignments/lab1/files/student.unl' 
 INTO TABLE STUDENT
 FIELDS TERMINATED BY '|';
 
